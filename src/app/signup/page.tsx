68e56f4c842d7ea809d0265c12afa3e8f51a39fa
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-08-03 00:17:11 +0530

    Creating Signup Route

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,62 +1,85 @@
 'use client';
 
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { axios } from 'axios';
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
 
 
 export default function () {
+    const router = useRouter();
     const [user, setUser] = useState({
         email: "",
         password: "",
         username: "",
     })
 
-    const onSignup = async () => {
+    const [buttonDisable, setButtonDisable] = useState(false)
+    const [loading, setLoading] = useState(false)
 
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0){
+            setButtonDisable(false);
+        } else {
+            setButtonDisable(true);
+        }
+    }, [user])
+
+    const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post('/api/users/signup' , user);
+            console.log("Signup Success: " , response.data);
+            router.push("/login");
+        } catch (error: any) {
+            console.log("Signup Failed: ", error.message)
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Signup</h1>
+            <h1>{loading ? "Please Wait..." : "Sign Up"}</h1>
 
             <hr />
 
             <label htmlFor="username">Username</label>
             <input
-                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus: border-gray-600'
+                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus: border-gray-600 text-black background-black appearance-none'
                 type="text"
                 name="username"
-                id="username" 
+                id="username"
                 placeholder='Username'
                 onChange={(e) => {
                     setUser({
                         ...user, username: e.target.value
                     })
                 }}
-            />
+                />
 
             <label htmlFor="email">Email</label>
             <input
-                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus: border-gray-600'
+                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus: border-gray-600 text-black background-black appearance-none'
                 type="text"
                 name="email"
-                id="email" 
+                id="email"
                 placeholder='Email'
                 onChange={(e) => {
                     setUser({
                         ...user, email: e.target.value
                     })
                 }}
-            />
+                />
 
             <label htmlFor="password">Password</label>
             <input
-                className='text-sky-400 p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus: border-gray-600'
+                className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus: border-gray-600 text-black background-black appearance-none'
                 type="password"
                 name="password"
-                id="password" 
+                id="password"
                 placeholder='Password'
                 onChange={(e) => {
                     setUser({
@@ -65,11 +88,11 @@ export default function () {
                 }}
             />
 
-            <button 
+            <button
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
                 onClick={onSignup}
             >
-                Signup Here!
+                {buttonDisable ? "Form Empty!" : "Sign Up"}
             </button>
 
             <Link href='/login'>Already have an account? Login instead</Link>
