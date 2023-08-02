86d1d76e0b5bfa87f773bcd7ffec8a56f9049682
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-08-02 16:53:57 +0530

    Configuring Database

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+export default async function () {
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+        connection.on('connected' , () => {
+            console.log(
+                "MongoDB Connected Successfully"
+            )
+        })
+
+        connection.on("error" , (error) => {
+            console.log("DB Side error. Make sure the database is connected correctly");
+            console.log(error);
+            process.exit();
+        })
+    } catch (error) {
+        console.log("Something went wrong!");
+        console.log(error);
+    }
+}
